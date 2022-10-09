e60cba04377ca8aac5d51e5b93d5c3a2b225d423
ZarakiLancelot <kenpachi2783@gmail.com>
2022-10-09 14:39:05 -0600

    Build navbar and include welcome controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,13 @@
 Rails.application.routes.draw do
+  get 'welcome/index'
+  get 'welcome/about'
+  get 'welcome/contact'
+  get 'welcome/pricing'
+  get 'welcome/features'
+  get 'welcome/faq'
   resources :invoices
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  root "welcome#index"
 end
